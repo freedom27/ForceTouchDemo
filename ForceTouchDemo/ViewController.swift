6e3b8183b606c03bd244df6ae45935a30e1d86ab
freedom <stefano.vettor@mac.com>
2015-11-03 23:48:19 +0100

    force touch implementation

--- ForceTouchDemo/ViewController.swift
+++ ForceTouchDemo/ViewController.swift
@@ -8,18 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension CAShapeLayer {
+    
+    func drawCircleAtLocation(location: CGPoint, withForce force: CGFloat, andBaseRadius baseRadius: CGFloat) {
+        let finalRadius = baseRadius*1.2 + (force * 70)
+        
+        let hue = (1.0-force) / 3.0
+        let baseColor = UIColor(hue: hue, saturation:1.0, brightness:1.0, alpha:1.0)
+        fillColor = baseColor.CGColor
+        
+        let origin = CGPoint(x: location.x - finalRadius, y: location.y - finalRadius)
+        path = UIBezierPath(
+            ovalInRect: CGRect(
+                origin: origin,
+                size: CGSize(width: finalRadius * 2, height: finalRadius * 2))).CGPath
+    }
+    
+}
 
+class ViewController: UIViewController {
+    
+    var circles = [UITouch: CAShapeLayer]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.multipleTouchEnabled = true
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        for touch in touches {
+            let circle = CAShapeLayer()
+            
+            let radius = touch.majorRadius + touch.majorRadiusTolerance
+            let forcePercentage = touch.force / touch.maximumPossibleForce
+            circle.drawCircleAtLocation(touch.locationInView(view),withForce: forcePercentage, andBaseRadius: radius)
+            circles[touch] = circle
+            view.layer.addSublayer(circle)
+        }
+        
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches where circles[touch] != .None
+        {
+            let circle = circles[touch]!
+            let radius = touch.majorRadius + touch.majorRadiusTolerance
+            let forcePercentage = touch.force / touch.maximumPossibleForce
+            circle.drawCircleAtLocation(touch.locationInView(view),withForce: forcePercentage, andBaseRadius: radius)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            let circle = circles[touch];
+            circle?.removeFromSuperlayer();
+            circles.removeValueForKey(touch)
+        }
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        guard let touches = touches else { return }
+        
+        for touch in touches where circles[touch] != .None {
+            let circle = circles[touch]!
+            circle.removeFromSuperlayer()
+            circles.removeValueForKey(touch)
+        }
+    }
+    
 }
 
